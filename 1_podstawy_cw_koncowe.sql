cf22dc39f82f1052054367f2fd44cc3398c8a71d
Kasl0 <slonieckacper@gmail.com>
2022-10-19 14:26:41 +0200

    Cwiczenia 3

--- 1_podstawy_cw_koncowe.sql
+++ 1_podstawy_cw_koncowe.sql
@@ -3,10 +3,10 @@
 --USE library SELECT DISTINCT member_no, fine_assessed FROM loanhist WHERE fine_assessed BETWEEN 8 AND 9
 --USE library SELECT title_no, author FROM title WHERE author = 'author' OR author = 'Jane Austen'
 --USE library SELECT title_no, title FROM title WHERE title LIKE '%adventures%'
---USE library SELECT DISTINCT member_no, fine_assessed, fine_paid FROM loanhist WHERE (fine_assessed IS NOT NULL AND fine_assessed != 0) AND (fine_paid IS NULL OR fine_paid = 0)
+--USE library SELECT member_no, fine_assessed, fine_paid, fine_waived FROM loanhist WHERE (ISNULL(fine_assessed, 0) - ISNULL(fine_paid, 0) - ISNULL(fine_waived, 0)) > 0
 --USE library SELECT DISTINCT city, [state] FROM adult
 
 --USE library SELECT * FROM title ORDER BY title
---USE library SELECT DISTINCT member_no, isbn, fine_assessed, fine_assessed * 2 AS 'double fine' FROM loanhist WHERE fine_assessed IS NOT NULL AND fine_assessed != 0
---USE library SELECT LOWER(firstname) + LOWER(middleinitial) + LOWER(SUBSTRING(lastname, 1, 2)) AS 'email_name' FROM member WHERE lastname = 'Anderson'
+--USE library SELECT DISTINCT member_no, isbn, fine_assessed, fine_assessed * 2 AS 'double fine' FROM loanhist WHERE ISNULL(fine_assessed, 0) > 0
+--USE library SELECT REPLACE(LOWER(firstname) + LOWER(middleinitial) + LOWER(SUBSTRING(lastname, 1, 2)), ' ', '') AS 'email_name' FROM member WHERE lastname = 'Anderson'
 --USE library SELECT 'The title is: ' + title + ', title number ' + CAST(title_no AS varchar) FROM title
--- 2_grupowanie.sql
+++ 2_grupowanie.sql
@@ -6,3 +6,24 @@
 --USE Northwind SELECT MAX(UnitPrice), MIN(UnitPrice), AVG(UnitPrice) FROM Products WHERE QuantityPerUnit LIKE '%bottle%'
 --USE Northwind SELECT * FROM Products WHERE UnitPrice > 28.8663
 --USE Northwind SELECT ROUND(SUM(UnitPrice * Quantity * (1-Discount)), 2) FROM [Order Details] WHERE OrderID = 10250
+
+--USE Northwind SELECT OrderID, MAX(UnitPrice * (1 - Discount)) AS 'Max' FROM [Order Details] GROUP BY OrderID
+--USE Northwind SELECT OrderID, MAX(UnitPrice * (1 - Discount)) AS 'Max' FROM [Order Details] GROUP BY OrderID ORDER BY Max
+--USE Northwind SELECT OrderID, MIN(UnitPrice * (1 - Discount)) AS 'Min', MAX(UnitPrice * (1 - Discount)) AS 'Max' FROM [Order Details] GROUP BY OrderID
+--USE Northwind SELECT ShipVia, COUNT(OrderID) FROM Orders GROUP BY ShipVia
+--USE Northwind SELECT TOP 1 ShipVia, COUNT(OrderID) AS 'Amount' FROM Orders WHERE Year(ShippedDate) = 1997 GROUP BY ShipVia ORDER BY Amount DESC
+
+--USE Northwind SELECT OrderID, COUNT(ProductID) FROM [Order Details] GROUP BY OrderID HAVING COUNT(ProductID) > 5
+--USE Northwind SELECT CustomerID, COUNT(OrderID) FROM Orders WHERE Year(ShippedDate) = 1998 GROUP BY CustomerID HAVING COUNT(OrderID) > 8 ORDER BY SUM(Freight) DESC
+
+--USE Northwind SELECT productid, orderid, SUM(quantity) AS total_quantity FROM orderhist GROUP BY productid, orderid WITH ROLLUP ORDER BY productid, orderid
+--USE Northwind SELECT productid, orderid, SUM(quantity) AS total_quantity FROM orderhist GROUP BY productid, orderid WITH CUBE ORDER BY productid, orderid
+
+--ile lat przepracował w firmie każdy z pracowników
+--USE Northwind SELECT EmployeeID, Year(HireDate) - Year() AS total_quantity FROM Employees GROUP BY productid, orderid WITH CUBE ORDER BY productid, orderid
+
+--policz sumę lat przepracowana przez wszystkich pracowników i średni czas w firmie
+--USE Northwind SELECT EmployeeID, Year(HireDate) - Year(GETDATE()) AS total_quantity FROM Employees GROUP BY productid, orderid WITH CUBE ORDER BY productid, orderid
+
+--policz z ilu liter składa się najkrótsze imie pracownika
+USE Northwind SELECT TOP 1 LEN(FirstName) FROM Employees ORDER BY LEN(FirstName)
